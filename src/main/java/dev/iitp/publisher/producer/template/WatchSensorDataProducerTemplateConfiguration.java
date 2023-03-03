84526eaf798aa9049572f3b99790636b4bf85bc3
goomon <jaegoo.jho@gmail.com>
2023-03-03 14:13:21 +0900

    Feat: Kafka producer metric setting

--- src/main/java/dev/iitp/publisher/producer/template/WatchSensorDataProducerTemplateConfiguration.java
+++ src/main/java/dev/iitp/publisher/producer/template/WatchSensorDataProducerTemplateConfiguration.java
@@ -1,6 +1,7 @@
 package dev.iitp.publisher.producer.template;
 
 import dev.iitp.publisher.model.SensorRecord;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.MicrometerProducerListener;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -21,17 +23,19 @@ public class WatchSensorDataProducerTemplateConfiguration {
     private final KafkaProperties properties;
 
     @Bean
-    public KafkaTemplate<String, SensorRecord> watchSensorDataKafkaTemplate() {
-        return new KafkaTemplate<>(watchSensorDateProducerFactory());
+    public KafkaTemplate<String, SensorRecord> watchSensorDataKafkaTemplate(ProducerFactory<String, SensorRecord> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 
     @Bean
-    public ProducerFactory<String, SensorRecord> watchSensorDateProducerFactory() {
-        return new DefaultKafkaProducerFactory<>(watchSensorDataProducerConfig());
+    public ProducerFactory<String, SensorRecord> watchSensorDateProducerFactory(MeterRegistry meterRegistry) {
+        Map<String, Object> props = watchSensorDataProducerConfig();
+        DefaultKafkaProducerFactory<String, SensorRecord> producerFactory = new DefaultKafkaProducerFactory<>(props);
+        producerFactory.addListener(new MicrometerProducerListener<>(meterRegistry));
+        return producerFactory;
     }
 
-    @Bean
-    public Map<String, Object> watchSensorDataProducerConfig() {
+    private Map<String, Object> watchSensorDataProducerConfig() {
         Map<String, Object> props = this.properties.buildProducerProperties();
         props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
